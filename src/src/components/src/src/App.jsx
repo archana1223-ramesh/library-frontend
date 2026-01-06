20c895de2abb0a7ef08eadffce3f13477b54c75f
Archanaramesh <archanaramesh1223@gmail.com>
2026-01-06 12:13:07 +0530

    Create App component for Library Book Management

---
+++ src/src/components/src/src/App.jsx
@@ -0,0 +1,12 @@
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div>
+      <h1>Library Book Management System</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
